c79ad7b0ec0a0a6c2baa85771af7a7f1903879ab
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-01-09 10:57:42 -0800

    created passportConfig.js and required passport and app.use on index.js

---
+++ config/passportConfig.js
@@ -0,0 +1,40 @@
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var db = require('../models'); //dealing with database in this file reference needed
+
+//define a way for the passport to serialize and deserialize users
+passport.serializeUser(function(user, callback){
+  callback(null, user.id);
+});
+
+//does opposite of funtion above
+passport.deserializeUser(function(id, callback) {
+  db.user.findById(id).then(function(user){
+    callback(null, user);
+  }).catch(function(err){ //this provides info about the error that occured
+    callback(err, null);
+  });
+});
+
+//define passport to actually do what we want it to do
+passport.use(new localStrategy({
+  //what fields do we want to use
+  usernameField: 'email',
+  passwordField: 'password'
+},  function(email, password, callback){
+  //look them up in the database
+  db.user.findOne({
+      where: { email: email }
+  }).then(function(user){
+    if(!user || !user.isValidPassword(password)){ //failure condition-didnt find user or entered the wrong password
+      callback(null, false);
+    }
+    else {
+      callback(null, user); //either user object or falsey value
+    }
+  }).catch(function(err){
+    callback(err, null); //error or null because there is no user
+  });
+}));
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var express = require('express');
 var ejsLayouts = require('express-ejs-layouts');
 var flash = require('connect-flash');
+var passport = require('./config/passportConfig');
 var session = require('express-session');
 var app = express();
 
@@ -19,6 +20,13 @@ app.use(session({
   saveUninitialized: true
 }));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function(req, res,next){
+  res.locals.currentUser = req.user;
+  res.locals.alerts = req.flash();
+  next();
+});
 
 app.get('/', function(req,res){
   res.render('home');
