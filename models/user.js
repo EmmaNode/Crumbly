6f2dcdcbf6a4d530b9e90a2267a49285412ee3fb
munimuni5”git config --global user.name munimuni5” <emileeleamons@gmail.com>
2018-01-09 10:46:45 -0800

    password validation made and bcrypt

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 'use strict';
+var bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   var user = sequelize.define('user', {
     firstname: DataTypes.STRING,
@@ -28,11 +30,31 @@ module.exports = (sequelize, DataTypes) => {
       }
     }
   }, {
+    hooks: {
+      beforeCreate: function(pendingUser, options){
+        if(pendingUser && pendingUser.password) //is this user not null and is the passwork not null string
+          var hash = bcrypt.hashSync(pendingUser.password, 10); //10 salt rounds is the default
+          pendingUser.password = hash; //reassigning password value of the object we are about to create || Changing the value of the password, the original string is overroad and done
+      }
+    },
     classMethods: {
       associate: function(models) {
         // associations can be defined here
       }
     }
   });
+
+user.prototype.isValidPassword = function(passwordTyped){
+  return bcrypt.compareSync(passwordTyped, this.password); //compares typed password to password stored in database that matches usernam
+}
+
+//this function deletes the password visiblity from user so it is not visivly posted anywhere
+user.prototype.toJSON = function(){
+  var user = this.get();
+  delete user.password;
+  return user;
+}
+
+
   return user;
 };
